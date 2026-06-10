c8201a11d644d47ef12343a38dfec7d8b2db411a
David Pitt <dpittmor@gmail.com>
2026-06-10 23:44:54 +0100

    feat: implement logout functionality with button in the main screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import { useAuthStore } from "@/store/authStore";
+import { useRouter } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet } from "react-native-unistyles";
+import "../unistyles";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    const router = useRouter();
+
+    const { logout } = useAuthStore();
+
+    const handleLogout = async () => {
+        await logout();
+
+        router.replace("/");
+    };
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Edit app/index.tsx to edit this screen.</Text>
+            <TouchableOpacity onPress={handleLogout}>
+                <Text style={styles.logOutText}>Log Out</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create((theme) => ({
+    logOutText: {
+        color: theme.colors.primary,
+        fontSize: theme.typography.sizes.h6,
+    },
+}));
